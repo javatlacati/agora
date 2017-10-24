52b5eb8bdf4edf66db8c4b78c596797c7c30c360
Mike Nabil <maikel_nabil@yahoo.com>
2017-10-24 12:18:30 -0400

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom'
+import logo from '../../logo.svg'
+import '../../styles/App.css'
+
+class App extends Component {
+  render () {
+    return (
+
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// React
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+
+// CSS
+import './styles/index.css'
+
+// Components
+import App from './components/App/App'
+
+// Render
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// Service Worker
+registerServiceWorker()
